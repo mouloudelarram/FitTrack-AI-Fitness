522185b238e1555de1ba76932cd472e7e13555f7
MOULOUD EL ARRAM <maoiloud2002@gmail.com>
2026-03-17 21:22:21 +0100

    feat: update detection function to save results

--- models/food detection/detect_food_withAI.py
+++ models/food detection/detect_food_withAI.py
@@ -1,23 +1,38 @@
 from ultralytics import YOLO
 from collections import Counter
+import cv2
+import os
 
 def count_food_items(image_path):
     # Load the model
-    model = YOLO("yolov8n.pt") 
+    model = YOLO("yolov8n.pt")
 
     # Run inference
     results = model.predict(source=image_path, conf=0.25, save=False, verbose=False)
 
     food_counts = {}
 
+    # Read original image
+    img = cv2.imread(image_path)
+
     for result in results:
-        # Extract all detected class names as a list
         detected_classes = [model.names[int(cls)] for cls in result.boxes.cls]
-        
-        # Count occurrences of each class
         food_counts = dict(Counter(detected_classes))
 
+        # Draw red bounding boxes
+        for box in result.boxes.xyxy:
+            x1, y1, x2, y2 = map(int, box)
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)  # red box
+
+    # Create output folder like YOLO
+    save_dir = "runs/predict"
+    os.makedirs(save_dir, exist_ok=True)
+
+    output_path = os.path.join(save_dir, os.path.basename(image_path))
+    cv2.imwrite(output_path, img)
+
     return food_counts
+
 
 # Usage
 my_plate = "apple.orange.jpg"
